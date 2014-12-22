b7aed681812d6aefd2963a0fc228047a48d90e6e
Em01 <emma_williams1@hotmail.co.uk>
2014-12-22 12:24:51 +0000

    defining the screen class

--- typechallenge.rb
+++ typechallenge.rb
@@ -1,1 +1,16 @@
-typechallenge.rb
+#Script name: Typing Challenge
+#Description: Demonstrating how to apply conditional logic in order to analyze user input and control
+#the execution of the script through a computer typing test.
+
+class Sceen
+
+	def cls
+		puts ("\n" * 25)
+		puts "\a"
+	end
+
+	def pause
+		STDIN.gets
+	end
+
+end
